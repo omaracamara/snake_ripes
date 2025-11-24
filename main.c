32c683b1e57632e35be3db87142adb238292f6ed
isaac-evs <isaac.vazquez@iteso.mx>
2025-11-24 00:39:43 -0600

    fixed dpad

--- main.c
+++ main.c
@@ -8,12 +8,15 @@
 
 
 // Pointer towards LED
-volatile int * LED_PTR = (volatile int *)0xF0000000;
+#define LED_PTR     ((volatile int *)0xF0000000)
 
 // Directional Pad
-volatile int * DPAD_RIGHT = (volatile int *)0xf0000dbc;
+#define DPAD_UP     ((volatile int *)0xF0000DB0)
+#define DPAD_DOWN   ((volatile int *)0xF0000DB4)
+#define DPAD_LEFT   ((volatile int *)0xF0000DB8)
+#define DPAD_RIGHT  ((volatile int *)0xF0000DBC)
 
-void SetBorders(white){
+void SetBorders(int white){
 
     int bottom_row_start = (LED_MATRIX_HEIGHT - 1) * LED_MATRIX_WIDTH;
 
@@ -35,44 +38,50 @@ void SetBorders(white){
         int right_offset = (y * LED_MATRIX_WIDTH + (LED_MATRIX_WIDTH - 1)) * PIXEL_SIZE_BYTES;
         *((volatile int *)((char *)LED_PTR + right_offset)) = white;
     }
-
-    return 0;
 }
 
 void _start() {
 
     // LED Matrix
-    int current_x = 1;
-    int current_y = 1;
+    int current_x = 7;
+    int current_y = 12;
 
-    // green HEX color
+    // HEX colors
     int green = 0x00FF00;
     int black = 0x000000;
     int white = 0xFFFFFF;
 
     SetBorders(white);
 
+    // initial position
     int start_offset = (current_y * LED_MATRIX_WIDTH + current_x) * PIXEL_SIZE_BYTES;
     *((volatile int *)((char *)LED_PTR + start_offset)) = green;
 
     // Loop forever
     while(1){
+        // change in positions
+        int dx = 0;
+        int dy = 0;
 
-        if(*DPAD_RIGHT != 0){
+        // check inputs
+        if (*DPAD_RIGHT) dx += 1;
+        if (*DPAD_LEFT)  dx -= 1;
+        if (*DPAD_DOWN)  dy += 1;
+        if (*DPAD_UP)    dy -= 1;
+
+        // If change detected
+        if(dx != 0 || dy != 0){
 
             // Calculate next position
-            int next_x = current_x + 1;
-            int next_y = current_y;
+            int next_x = current_x + dx;
+            int next_y = current_y + dy;
 
             // Calculate Address of next pixel
             int next_offset = (next_y * LED_MATRIX_WIDTH + next_x) * PIXEL_SIZE_BYTES;
             volatile int * next_ptr = (volatile int * )((char*)LED_PTR + next_offset);
 
-            // Read pixel color
-            int pixel_color = *next_ptr;
-
-            // If its not white = safe
-            if(pixel_color != white){
+            // Check Colition
+            if(*next_ptr != white){
 
                 // Erease trail
                 int old_offset = (current_y * LED_MATRIX_WIDTH + current_x) * PIXEL_SIZE_BYTES;
@@ -80,14 +89,14 @@ void _start() {
 
                 // Update coordinate
                 current_x = next_x;
+                current_y = next_y;
 
+                // Draw character
                 *next_ptr = green;
             }
 
             // Debouncing
-            while(*DPAD_RIGHT != 0){
-
-            }
+            while(*DPAD_RIGHT || *DPAD_LEFT || *DPAD_UP || *DPAD_DOWN) {}
         }
 
     }
